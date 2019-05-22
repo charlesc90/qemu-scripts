2bcdf181cf64a77418118d208e857b61d1014d3d
Charles Cox <charles.h.cox@gmail.com>
2019-05-21 19:41:52 -0500

    Update template.sh

--- template.sh
+++ template.sh
@@ -2,11 +2,16 @@
 
 # This script uses qemu and KVM to spin up a VM
 # qemu-kvm [options] [disk_image]
-# Ncurses-based interface to initialize variables
+# adjust these variables according to your environment
+# q35 is the newest chipset available for x86_64
+# there are several bioses/efis in the /usr/share/qemu directory
 
-# Initialize the VM
-qemu-kvm -machine q35 -cpu host -m size=2048M,slots=2,maxmem=4096M          \
-    -blockdev driver=qcow2,node-name=hda,file=/home/charlesc/kvm/qemu.qcow2 \
-    -vga virtio                                                             \
-    -netdev bridge,id=n1 -device virtio-net,netdev=n1                       \
-    -boot c
+chipset="q35"
+vcpu="host"
+memories="2048M"
+gpu="qxl"
+efi="/usr/share/qemu/ovmf-x86_64-opensuse.bin"
+boots="openSUSE-Tumbleweed-JeOS.x86_64-15.1.0-kvm-and-xen-Build4.1.qcow2"
+
+# initialize the vm
+qemu-kvm -machine ${chipset} -cpu ${vcpu} -m ${memories} -vga ${gpu} -bios ${efi} -boot d ${boots}
