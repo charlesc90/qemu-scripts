a37f34b081e47ed9e7f11ffe37dd249a0996e755
Charles Cox <charles.h.cox@gmail.com>
2019-08-02 13:55:56 -0500

    Update win10.sh

--- win10.sh
+++ win10.sh
@@ -4,7 +4,7 @@ chipset="type=q35,kernel_irqchip=on,mem-merge=on"
 vcpu="host"
 hyper="kvm,thread=multi"
 memories="8192M"
-bios="/usr/share/qemu/ovmf-x86_64-ms-4m.bin"
+efi="/home/charlesc/kvm/ovmf-x86_64.bin"
 gpu="virtio"
 gpuaccel="gtk,gl=on"
 nets="user,ipv6=off,model=virtio"
@@ -12,4 +12,5 @@ dvd="/home/charlesc/kvm/win10.iso"
 disk="/home/charlesc/kvm/win10.qcow2"
 
 qemu-kvm -machine ${chipset} -cpu ${vcpu} -accel ${hyper} -m ${memories} \
-  -vga ${gpu} -display ${gpuaccel} -nic ${nets} -cdrom ${dvd} ${disk}
+  -bios ${efi} -vga ${gpu} -display ${gpuaccel} -nic ${nets}             \
+  -cdrom ${dvd} -boot d ${disk}
