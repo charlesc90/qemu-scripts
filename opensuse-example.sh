2990fbd9dd9884d90e79df60e9e7824299a840d1
Charles Cox <charles.h.cox@gmail.com>
2023-02-10 15:40:29 -0600

    Create opensuse-example.sh

---
+++ opensuse-example.sh
@@ -0,0 +1,25 @@
+chipset="type=q35,kernel_irqchip=on,mem-merge=on"
+vcpu="host"
+multi="4,sockets=4,cores=1,threads=1"
+hyper="kvm"
+memories="8192M"
+efi="/home/charlesc/pool/OVMF_CODE.fd"
+gpu="virtio"
+gpuaccel="gtk,gl=on"
+nets="user,model=virtio-net-pci"
+sound="alsa,id=example,out.channels=1"
+disk="openSUSE-Tumbleweed-Minimal-VM.x86_64-kvm-and-xen.qcow2"
+
+qemu-kvm					          \
+	-machine ${chipset} 			\
+	-cpu ${vcpu}				      \
+	-smp ${multi}				      \
+	-accel ${hyper} 			    \
+	-m ${memories}   			    \
+	-nic ${nets}				      \
+	-pflash ${efi}				    \
+  -vga ${gpu} 				      \
+  -display ${gpuaccel} 		  \
+  -nic ${nets}				      \
+  -audiodev ${sound}        \              \
+  -boot d ${disk}		
