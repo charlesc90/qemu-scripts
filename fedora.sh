19d42a94160fd3f30c5cb1a484097ef809c4371a
Charles Cox <charles.h.cox@gmail.com>
2023-05-13 16:09:21 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -2,13 +2,13 @@
 
 chipset="type=q35,kernel_irqchip=on,mem-merge=on"
 vcpu="host"
-multi="2,sockets=2,cores=1,threads=1"
-hyper="kvm"
+multi="4,sockets=1,cores=4,threads=1"
 
 efi="node-name=rom,driver=file,filename=/home/charlesc/qemu/OVMF_CODE.fd,read-only=true"
 vars="node-name=efivars,driver=file,filename=/home/charlesc/qemu/OVMF_VARS.fd"
 
 nd="user,ipv6=off,model=virtio-net-pci,mac=52:54:98:76:54:32"
+
 
 ipm="ipmi-bmc-sim,id=ipmi0"
 ik="isa-ipmi-kcs,bmc=ipmi0"
@@ -18,17 +18,17 @@ gpuaccel="gtk,gl=off"
 
 hd="/home/charlesc/qemu/Fedora-Server-KVM-38-1.6.x86_64.qcow2"
 
-qemu-kvm		            \
+qemu-kvm		            	\
 	-machine ${chipset}		\
-	-cpu ${vcpu}			    \
-	-m 1024M				      \
-	-smp ${multi}			    \
-	-accel ${hyper}			  \
-	-blockdev ${efi}		  \
-	-blockdev ${vars}		  \
-	-nic ${nd}				    \
-    -device ${ipm}			\
-    -device ${ik}			  \
-	-vga ${gpu}				    \
-	-display ${gpuaccel}	\
-	-hda ${hd}				    \
+	-cpu ${vcpu}			\
+	-m 8192M			\
+	-smp ${multi}			\
+	-accel ${hyper}			\
+	-blockdev ${efi}		\
+	-blockdev ${vars}		\
+	-nic ${nd}			\
+    	-device ${ipm}			\
+    	-device ${ik}			\
+	-vga ${gpu}			\
+	-display ${gpuaccel}		\
+	-hda ${hd}			\
