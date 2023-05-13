b1b5bb782e2596e32189c18eed4b2793bfce07b1
Charles Cox <charles.h.cox@gmail.com>
2023-05-13 14:24:18 -0500

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+chipset="type=q35,kernel_irqchip=on,mem-merge=on"
+vcpu="host"
+multi="2,sockets=2,cores=1,threads=1"
+hyper="kvm"
+
+efi="node-name=rom,driver=file,filename=/home/charlesc/qemu/OVMF_CODE.fd,read-only=true"
+vars="node-name=efivars,driver=file,filename=/home/charlesc/qemu/OVMF_VARS.fd"
+
+nd="user,ipv6=off,model=virtio-net-pci,mac=52:54:98:76:54:32"
+
+ipm="ipmi-bmc-sim,id=ipmi0"
+ik="isa-ipmi-kcs,bmc=ipmi0"
+
+gpu='virtio'
+gpuaccel="gtk,gl=off"
+
+hd="/home/charlesc/qemu/Fedora-Server-KVM-38-1.6.x86_64.qcow2"
+
+qemu-kvm		            \
+	-machine ${chipset}		\
+	-cpu ${vcpu}			    \
+	-m 1024M				      \
+	-smp ${multi}			    \
+	-accel ${hyper}			  \
+	-blockdev ${efi}		  \
+	-blockdev ${vars}		  \
+	-nic ${nd}				    \
+    -device ${ipm}			\
+    -device ${ik}			  \
+	-vga ${gpu}				    \
+	-display ${gpuaccel}	\
+	-hda ${hd}				    \
