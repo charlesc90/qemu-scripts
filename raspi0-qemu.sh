bf48cc2536f8f693f926440f7fdb0800a934fac9
Charles Cox <charles.h.cox@gmail.com>
2023-05-13 22:15:38 -0500

    raspi0-qemu.sh

--- raspi0-qemu.sh
+++ raspi0-qemu.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
-qemu-system-arm
-  -machine raspi0 -kernel kernel.img -dtb bcm2708-rpi-zero-w.dtb -no-reboot -usbdevice keyboard								                                  \
-  -append "earlycon=pl011,0x20201000 console=ttyAMA1,115200 root=PARTUUID=4c4e106f-02 initcall_blacklist=bcm2835_pm_driver_init rw rootwait" 		\
-  -drive "file=pi.img,format=raw,index=0,media=disk"
+qemu-system-arm \
+	-machine raspi0	\
+	-kernel kernel.img \
+	-dtb bcm2708-rpi-zero-w.dtb \
+	-device usb-kbd \
+	-no-reboot \
+	-append "earlycon=pl011,0x20201000 console=ttyAMA0,115200  dwc_otg.fiq_fsm_enable=0 dwc_otg.fiq_enable=0 dwc_otg.nak_holdoff=0 root=/dev/mmcblk0p2 initcall_blacklist=bcm2835_pm_driver_init arm_boost=1 rootwait" \
+	-drive "file=pi.img,format=raw,index=0,media=disk"
